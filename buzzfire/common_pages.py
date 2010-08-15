5c46a9cd181af915fe18ed870af9625ced1c6ab5
Kenny Shen Renhui <kenny@northpole.sg>
2010-08-15 14:45:06 +0800

    add mybuzz button, updated templates/css

--- buzzfire/common_pages.py
+++ buzzfire/common_pages.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
+from buzzfire.utils.utils import check_auth
+
 def homepage(request):
-	return render_to_response('common_pages/homepage.html', {}, context_instance=RequestContext(request))
+	auth_status = check_auth(request)
+	
+	if auth_status:
+		buzz_auth = True
+	else:
+		buzz_auth = None
+		
+	return render_to_response('common_pages/homepage.html', {'buzz_auth':buzz_auth}, context_instance=RequestContext(request))
 	
 def faq(request):
-	return render_to_response('common_pages/faq.html', {}, context_instance=RequestContext(request))
+	auth_status = check_auth(request)
+	
+	if auth_status:
+		buzz_auth = True
+	else:
+		buzz_auth = None
+		
+	return render_to_response('common_pages/faq.html', {'buzz_auth':buzz_auth}, context_instance=RequestContext(request))
