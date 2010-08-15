1acaca3d1c14ba81c83fcb746e340fb034f2da1a
Kenny Shen Renhui <kenny@northpole.sg>
2010-08-15 14:28:58 +0800

    signout button + updates to template/css

--- buzzfire/twitter_app/views.py
+++ buzzfire/twitter_app/views.py
@@ -94,7 +94,7 @@ def mybuzz(request):
 		buzz_auth = True
 	else:
 		buzz_auth = None
-	# Get user's timeline
+	
 	return render_to_response('mybuzz/homepage.html', {'buzz_auth':buzz_auth}, context_instance=RequestContext(request))
 
 # DATA GET VIEWS
