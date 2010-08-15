ae427703fc8a0b8214f81cf2413ba677ea59d34d
Choon Kee Oh <choonkeeoh@mettaabode.gateway.2wire.net>
2010-08-15 10:33:51 +0800

    fixed bugs with urls.py

--- buzzfire/urls.py
+++ buzzfire/urls.py
@@ -13,10 +13,10 @@ urlpatterns = patterns('',
 	(r'^login/$', 'buzzfire.twitter_app.views.login'),
 	(r'^logout/$', 'buzzfire.twitter_app.views.logout'),
 
-        (r'^comment/add/$' 'buzzfire.comment_app.views.add'),
-        (r'^comment/get/(\w+)$' 'buzzfire.comment_app.views.get'),
-        (r'^comment/get/bookmark/(\w+)$' 'buzzfire.comment_app.views.get_bookmark_comment'),
-        (r'^comment/delete/(\w+)$' 'buzzfire.comment_app.views.delete'),
+        (r'^comment/add/$', 'buzzfire.comment_app.views.add'),
+        (r'^comment/get/(\w+)$', 'buzzfire.comment_app.views.get'),
+        (r'^comment/get/bookmark/(\w+)$', 'buzzfire.comment_app.views.get_bookmark_comment'),
+        (r'^comment/delete/(\w+)$', 'buzzfire.comment_app.views.delete'),
          
 	
 	# user_page
